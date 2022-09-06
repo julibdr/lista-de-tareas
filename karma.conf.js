61c5297eafdd1e5a68933f1368607af477a2e7c3
julieta <julietabdr@icloud.com>
2022-09-06 16:20:36 -0300

    fixes2

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-todos'),
+      dir: require('path').join(__dirname, './coverage/lista-de-tareas'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
